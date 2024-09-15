bb1ee77ab0ff260f8ca88c0482267ea556b3418e
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-09-14 22:04:18 -0600

    Time: 98 ms (97.68%), Space: 17.7 MB (51.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        actual = sum(nums)
+        n = len(nums)
+        total_sum = n * (n + 1)//2 
+        return total_sum - actual
 
     #Sort the Array - O(nlogn)
     # Find the largest element - O(n)
@@ -8,26 +12,26 @@ class Solution:
     #     Then decrement - O(n)
     # if not length of array is equal to the largest  - O(1)
     #     return largest + 1
-        if len(nums) == 1:
-            if nums[0] == 0:
-                return 1
-            elif nums[0] == 1:
-                return 0
+        # if len(nums) == 1:
+        #     if nums[0] == 0:
+        #         return 1
+        #     elif nums[0] == 1:
+        #         return 0
         
         # return nums[0]
-        nums.sort()
-        largestElement = len(nums) # 3
-        largest = max(nums) # 3
-        if largestElement == largest:
-            # largest -= 1
-            for i, val_i in enumerate(nums):
-                if nums[len(nums) - i - 1] == largest:
-                    largest -= 1
-                else:
-                    return largest
-            return 0
-        else:
-            return largest + 1
+        # nums.sort()
+        # largestElement = len(nums) # 3
+        # largest = max(nums) # 3
+        # if largestElement == largest:
+        #     # largest -= 1
+        #     for i, val_i in enumerate(nums):
+        #         if nums[len(nums) - i - 1] == largest:
+        #             largest -= 1
+        #         else:
+        #             return largest
+        #     return 0
+        # else:
+        #     return largest + 1
 
                 
         
