e4c40f78ce86da8c6a3331b860410fed801aabc4
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-09-16 10:48:09 -0600

    Time: 64 ms (33.95%), Space: 16.7 MB (22.33%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -7,7 +7,6 @@ class Solution:
         if len(magazine) < len(ransomNote): return False
         # if they are same size and not equal, return false
         if len(magazine) == len(ransomNote) and sorted(magazine) != sorted(ransomNote): return False
-
         rnCount = [0] * 26
         magCount = [0] * 26
         # make a array that houses all the letters and counts there are
@@ -18,21 +17,15 @@ class Solution:
         for i in ransomNote:
             rnCount[ord(i) - ord("a")] += 1
         print("ransomNote",  rnCount)
-
         # if the elements for each are the same, and the count for mag is equal or greater than 
         # ransomeCount, return True
-        for i, val in enumerate(range(len(magCount)-1)):
+        for i in range(len(magCount)-1):
             checkVal = magCount[i] - rnCount[i]
             print("checkVal", checkVal)
             if checkVal < 0:
                 return False
         return True
-
         # a b c d e f g h i j k l m n o p q r s t u v w z y z   count
         # 6 3 1 2 3 2 2 2 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0   magazine
         # 0 0 0 0 1 1 0 1 2 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0   ransomeNote
         # 6 3 1 2 2 1 2 2 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0     total 
-
-            # if ransomNote[val] <= magazine[val]:
-            #     print("ransomVal" , ransomNote[i], "magVal" , magazine[i])
-            #     return True
