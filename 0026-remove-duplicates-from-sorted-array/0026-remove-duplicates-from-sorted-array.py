405f9ce762ab417c3aef691abecc399ec6b4b916
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-09-29 16:52:21 -0600

    Time: 170 ms (5%), Space: 18.1 MB (11.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,26 +1,43 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        # Start count at 1 because the first element is always unique
-        count = 1
-        n = len(nums)
-
-        # Loop through the array starting from the second element
-        for i in range(1, n):
-            if nums[i] != nums[i - 1]:  # Compare current element with the previous one
-                nums[count] = nums[i]  # Place unique element at `count`
+        count = 0
+        #  0  1  2  3   4  5
+        # [ 1, 1, 2, 2, 4 ,4 ]
+        #   i
+        #         j
+        i = 0
+        j = 0
+        while j < len(nums):
+            print("i", i, "nums[i]", nums[i])
+            print("j", j , "nums[j]", nums[j])
+            if nums[i] == nums[j]:
+                j += 1
+                print("j again", j)
+            else:
+                print("IN ELSE!! i", i, "nums[i]", nums[i])
+                print("in else!!!!! j", j , "nums[j]", nums[j])
+                nums[i+1] = nums[j]
                 count += 1
+                i += 1
+            # i = j
+        print(count)
+        return count + 1
 
-        return count
-        # nums.sort() # O(N)
-        # left = 0
-        # right = len(nums)-1
+        # my_dict = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in my_dict:
+        #         my_dict[nums[i]] = i
+        #     # else:
+        #     #     my_dict[nums[i]] += i
+        # print(my_dict)
+
+        # [1 , 1, 2]
+        # left = 0 
+        # right = len(nums)
         # while left < right:
-        #     if nums[left] == nums[right]:
-        #         nums.pop(left)
-        #         left += 1
-        #     else:
+        #     if nums[left] != nums[right - 1]:
+        #         nums[left] = nums[right -1]
         #         right -= 1
-        
+        #     else:
+        #         left += 1
+        # return right
