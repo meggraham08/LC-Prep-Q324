28d1d33d72a9139ef60748927925503ba25e3d7b
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-10-12 14:52:46 -0600

    Time: 37 ms (76.9%), Space: 16.5 MB (48.19%) - LeetHub

--- 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.py
+++ 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.py
@@ -2,12 +2,14 @@ class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
         # [ 1, 2, 3 ].        [ 3, 2, 7 ]
         # 
-        # 
-        # 
-        # loop through i and loop through j, if query time is in between the range for nums1[i] and nums2[i]
+        # loop through startTime and loop through endTime,
+        # if query time is in between the range for nums1[i] and nums2[i]
         # count += 1
         count = 0
+        # can do this because startTime.len is the same as endTime.len
         for i in range(len(startTime)):
+            # this is literally checkinf if its in the range
+            # so is 1 < = queryTime <= 3:
             if startTime[i] <= queryTime <= endTime[i]:
                 count += 1
         return count
