72e21c01dd354a0f9fed064b5e021e14fbcd2c8b
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-10-31 08:47:59 -0600

    Time: 94 ms (10.84%), Space: 16.9 MB (86.89%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -3,12 +3,15 @@ class Solution:
         unique = {}
         #brute force
         # {'l': 1, 'e': 3, 't': 1, 'c': 1, 'o': 1, 'd': 1}
-        # 
+        #  create hash map
+        # loop through to find the value one, immediately return the index
+        # since you are looping through s!
         for i in range(len(s)):
             if s[i] in unique:
                 unique[s[i]] += 1
             else:
                 unique[s[i]] = 1
+        # loop through string, check if the value of 
         for i, val_i in enumerate(s):
             if unique[val_i] == 1:
                 return i
