53e730b522e7a23aba98ce47df0ea2a22fca3420
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-11-05 07:58:57 -0700

    Time: 298 ms (16.4%), Space: 24.1 MB (39.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-              
-        count = 0
         visited = set()
-        
+        count = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == '1' and (r, c) not in visited:
-                    self.dfs(grid, r, c, visited)
-                    count += 1  # New island found
-        
+                if self.dfs(grid, r, c, visited) == True:
+                    count += 1
         return count
-    
+
     def dfs(self, grid, r, c, visited):
         row_inbounds = 0 <= r < len(grid)
         col_inbounds = 0 <= c < len(grid[0])
+
+        if not row_inbounds or not col_inbounds or grid[r][c] == '0' or (r,c) in visited:
+            return False
+        visited.add((r,c))
         
-        if not row_inbounds or not col_inbounds or grid[r][c] == '0' or (r, c) in visited:
-            return
-        
-        visited.add((r, c))
-        
-        self.dfs(grid, r - 1, c, visited)  # Up
-        self.dfs(grid, r + 1, c, visited)  # Down
-        self.dfs(grid, r, c - 1, visited)  # Left
-        self.dfs(grid, r, c + 1, visited)  # Right
+        self.dfs(grid, r + 1, c, visited)
+        self.dfs(grid, r - 1, c, visited)
+        self.dfs(grid, r , c - 1, visited)
+        self.dfs(grid, r, c + 1, visited)
+        return True
