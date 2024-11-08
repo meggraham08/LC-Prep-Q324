0c3909ad5b5d7bbf7c9fa286a708c81327538e28
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-11-08 16:32:05 -0700

    Time: 0 ms (100%), Space: 17.5 MB (11.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -4,18 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        def fill_levels(root, levels, height):
+        # mutatue levels list
+            if root is None:
+                return
+                # if len of levels ( 0 == 0)
+            # we add a new array with the value
+            # otherwise we add the node to the value with the height its supposed to be
+            if len(levels) == height:
+                levels.append([root.val])
+            else:
+                levels[height].append(root.val)
+            fill_levels(root.left, levels, height + 1)
+            fill_levels(root.right, levels, height + 1)
         levels = []
-        self.fill_levels(root, levels, 0)
+        fill_levels(root, levels, 0)
         return levels
-    def fill_levels(self,root, levels, height):
-        if root is None:
-            return None
-        if len(levels) == height:
-            levels.append([root.val])
-        else:
-            levels[height].append(root.val)
-        self.fill_levels(root.left, levels, height + 1)
-        self.fill_levels(root.right, levels, height + 1)
