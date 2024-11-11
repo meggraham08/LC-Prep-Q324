4bbec4d6a056e5411b2b256e246e8dddd0833e63
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-11-11 12:07:26 -0700

    Time: 117 ms (66.26%), Space: 16.7 MB (29.67%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,13 +1,9 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        count = 0
-        
-        # Iterate through all possible palindrome centers
+        count = 0        
         for center in range(2 * len(s) - 1):
             left = center // 2
             right = left + center % 2
-            
-            # Expand around the center to find palindromic substrings
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 count += 1
                 left -= 1
