6a415d1ae94abb1b8992e75e0fcb7d6369c941e7
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-11-10 20:24:44 -0700

    Time: 45 ms (9.7%), Space: 17.5 MB (41.25%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -6,19 +6,19 @@ class Solution:
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 size = self.dfs(grid, r, c, visited)
-                if size > max_size:
-                    max_size = size
+                max_size = max(max_size, size)
+                # if size > max_size:
+                #     max_size = size
         return max_size
 
     def dfs(self, grid, r, c, visited):
+        size = 1
         pos = (r, c)
         row_inbounds = 0 <= r < len(grid)
         col_inbounds = 0 <= c < len(grid[0])
         if not row_inbounds or not col_inbounds or grid[r][c] == 0 or pos in visited:
             return 0
         visited.add(pos)
-        size = 1
-
         size += self.dfs(grid, r + 1, c, visited)
         size += self.dfs(grid, r - 1, c, visited)
         size += self.dfs(grid, r , c + 1, visited)
