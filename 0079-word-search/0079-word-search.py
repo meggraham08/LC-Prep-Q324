485e33f90e3ac8b867ffb1486d9ea45201601a68
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-11-12 09:55:40 -0700

    Time: 4809 ms (31.12%), Space: 16.7 MB (57.74%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,17 +3,23 @@ from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
+        visited = set()
         
         def dfs(r, c, index):
-            # Check if we've matched all characters in the word
-            if index == len(word):
-                return True
-            # Check out of bounds, mismatch, or already visited cell
-            if r < 0 or c < 0 or r >= m or c >= n or board[r][c] != word[index]:
+            # Check row and column bounds
+            row_inbounds = 0 <= r < m
+            col_inbounds = 0 <= c < n
+            
+            # If out of bounds, character mismatch, or cell already visited, return False
+            if not row_inbounds or not col_inbounds or board[r][c] != word[index] or (r, c) in visited:
                 return False
             
-            # Mark the cell as visited by temporarily changing its value
-            temp, board[r][c] = board[r][c], '#'
+            # If all characters are matched, return True
+            if index == len(word) - 1:
+                return True
+            
+            # Mark the cell as visited by adding to the visited set
+            visited.add((r, c))
             
             # Explore neighbors in the four cardinal directions
             found = (dfs(r + 1, c, index + 1) or
@@ -21,8 +27,8 @@ class Solution:
                      dfs(r, c + 1, index + 1) or
                      dfs(r, c - 1, index + 1))
             
-            # Unmark the cell (backtrack) by restoring its original value
-            board[r][c] = temp
+            # Unmark the cell (backtrack) by removing from visited
+            visited.remove((r, c))
             return found
 
         # Start DFS from each cell on the board
