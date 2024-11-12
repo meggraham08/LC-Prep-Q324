db44e2eb9da45b32cb85436f29da522789a9b25d
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2024-11-12 09:23:10 -0700

    Time: 302 ms (52.21%), Space: 16.9 MB (22.47%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,22 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         resLen = 0
-        res = ''
-        for i in range(len(s)):
-            # odd len
-            l, r = i, i
+        res = ""
+        
+        def checkPal(l, r):
+            nonlocal resLen, res
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
-                    res = s[l : r + 1]
-                    resLen = r - l + 1
-                l -= 1
-                r += 1
-            # even len
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
+                currLen = r - l + 1
+                if currLen > resLen:
                     res = s[l:r + 1]
-                    resLen = r - l + 1
+                    resLen = currLen
                 l -= 1
                 r += 1
+        
+        for i in range(len(s)):
+            # Check for odd-length palindromes
+            checkPal(i, i)
+            # Check for even-length palindromes
+            checkPal(i, i + 1)
+        
         return res
