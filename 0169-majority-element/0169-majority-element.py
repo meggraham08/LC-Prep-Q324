32d2f115ee41011519aeacf4ad6dcc22932cfd85
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2025-01-17 11:51:16 -0700

    Time: 11 ms (31.67%), Space: 19.3 MB (28.17%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        majEle = len(nums) // 2
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        print(count)
+        for key, val in count.items():
+            if val > majEle:
+                return key
+        
