4dacb41b0303918e3320e8d24e3a6bcfa96701b9
Meg Graham <25336933+meggraham08@users.noreply.github.com>
2025-01-20 15:34:24 -0700

    Time: 0 ms (100%), Space: 18.1 MB (9.6%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -1,27 +1,22 @@
-from collections import deque
+import threading
+
 class HitCounter:
 
     def __init__(self):
-        self.hits = [] # can't remove hits, we need indexes
-        
+        self.hits = []
+        self.lock = threading.Lock()
 
     def hit(self, timestamp: int) -> None:
         self.hits.append(timestamp)
 
     def getHits(self, timestamp: int) -> int:
-        left, right = 0, len(self.hits) -1
-
-        target = timestamp - 300
-
-        while left <= right:
-            mid = (left + right) // 2
-            if self.hits[mid] <= target:
-                left = mid + 1
-            else:
-                right = mid - 1
+        with self.lock:
+            target = timestamp - 300
+            left, right = 0, len(self.hits) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if self.hits[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return len(self.hits) - left
-
-# Your HitCounter object will be instantiated and called as such:
-# obj = HitCounter()
-# obj.hit(timestamp)
-# param_2 = obj.getHits(timestamp)
